82dee37b747f40836e26c4cdd4c4a20885820663
Benno Kruit <bennokr@gmail.com>
2014-06-22 23:08:20 +0200

    finished hyperparameters

--- tensortree.py
+++ tensortree.py
@@ -4,6 +4,7 @@ import numpy as np
 
 def tensorLayer((l,r), (T, M, b), nl):
     """ Compute Inner Tensor Layer """
+    print l
     return nl(l.T.dot(T).dot(r) + M.dot(np.append(l,r)) + b)
 
 def tensorGrad ((l,r), (T, M, b), delta, nld, output):
@@ -16,29 +17,30 @@ def tensorGrad ((l,r), (T, M, b), delta, nld, output):
     delta_r = (l.dot(T).T + M[:,M.shape[1]/2:].T).dot(g)
     return (gT, gM, gb), (delta_l, delta_r)
 
-def ntn_dtype(i, o):
-    """ The dtype of the parameters of a Neural Tensor Network"""
-    return np.dtype([('b', '<f8', (o,)),       # bias
-                     ('M', '<f8', (o, 2 * i)), # matrix
-                     ('T', '<f8', (o, i, i))]) # tensor
-
-def random_dtype(dt):
-    """ Random numpy array for a given dtype"""
-    return np.array(tuple([random.randn(*a[2]) for a in dt.descr]), dtype=dt)
+def init_ntn_params(i,o):
+    """ Random Neural Tensor Network parameters"""
+    return (
+        np.random.randn(o,i,i), # T
+        np.random.randn(o,2*i), # M
+        np.random.randn(o)      # b
+    )
 
 class Leaf():
-    def __init__(self, vec, index=0, hyp, param):
-        self.vec = vec
+    def __init__(self, index):
         self.i = index
+        self.vec = None # store activation
 
-    def do(self):
+    def do(self, hyp, param):
+        """ Look up word in vocabulary """
+        self.vec = param.vocab[self.i]
         return self.vec
     
-    def grad(self, delta):
+    def grad(self, delta, output, hyp, param):
         """ Create the gradients of a node """
-        global T, M, b, W
+        (T, M, b) = param.composition
         gb, gM, gT = np.zeros(b.shape), np.zeros(M.shape), np.zeros(T.shape)
-        gW = dok_matrix(W.shape) # Keep word gradients in a sparse array
+        # Keep word gradients in a sparse array, so we can add them up
+        gW = dok_matrix((hyp.vocab_size, hyp.word_size))
         gW[self.i] = delta
         return ((gT, gM, gb), gW)
 
@@ -48,55 +50,55 @@ class Tree(Leaf):
         self.right = right
         self.vec = None # store activation
         
-    def do(self):
+    def do(self, hyp, param):
         """ Forward pass """
-        global T, M, b, nl
-        l, r = self.left.do(), self.right.do()
-        self.vec = tensorLayer((l,r), (T, M, b), nl)
+        l, r = self.left.do(hyp, param), self.right.do(hyp, param)
+        self.vec = tensorLayer((l,r), param.composition, hyp.composition_transfer)
         return self.vec
     
-    def grad(self, delta, output):
+    def grad(self, delta, output, hyp, param):
         """ Get the gradients of this tree """
-        global T, M, b, nld
         l, r = self.left.vec, self.right.vec
         
         (gT, gM, gb), (delta_l, delta_r) = \
-            tensorGrad ((l,r), (T, M, b), delta, nld, output)
+            tensorGrad ((l,r), param.composition, delta, hyp.composition_backtrans, output)
 
-        ((gTl, gMl, gbl), gWl) = self.left.grad(delta_l)
-        ((gTr, gMr, gbr), gWr) = self.right.grad(delta_r)
+        ((gTl, gMl, gbl), gWl) = self.left.grad (delta_l, l, hyp, param)
+        ((gTr, gMr, gbr), gWr) = self.right.grad(delta_r, r, hyp, param)
         
         return ((gTl + gTr + gT, gMl + gMr + gM, gbl + gbr + gb), gWl + gWr)
 
 class SGD():
-    def __init__(self, hyp, param = None):
+    def __init__(self, hyp):
         self.hyp = hyp
-        if param:
-            self.param = param
 
-    def cost_and_grad(self, left_tree, right_tree, true_relation):
+    def cost_and_grad(s, left_tree, right_tree, true_relation, param):
         def normalize(v):
             norm = np.linalg.norm(v)
             return v / norm if norm else v
 
+        nl  = hyp.comparison_transfer
+        nld = hyp.comparison_backtrans
+        S = param.softmax
+
         ## Run forward
-        l, r = left_tree.do(), right_tree.do()
-        comparison = tensorLayer((l,r), (T2, M2, b2), nl2)
+        l, r = left_tree.do(s.hyp, param), right_tree.do(s.hyp, param)
+        comparison = tensorLayer((l,r), param.comparison, nl)
         softmax = normalize(np.exp( S.dot(np.append(1, comparison)) ))
 
         cost = -np.log(softmax[true_relation])
         
         ## Get gradients
         # softmax
-        diff = softmax - np.eye(c1)[true_relation]
-        delta = ( nld2(np.append(1, comparison)) * S.T.dot(diff) )[1:]
+        diff = softmax - np.eye(hyp.classes)[true_relation]
+        delta = ( nld(np.append(1, comparison)) * S.T.dot(diff) )[1:]
         gS = np.append(1, comparison) * diff[:, None]
         # comparison
         (gT2, gM2, gb2), (delta_l, delta_r) = \
-                tensorGrad ((l,r), (T2, M2, b2), delta, nld2, comparison)
+                tensorGrad ((l,r), param.comparison, delta, nld, comparison)
         # composition
-        ((gTl, gMl, gbl), gWl) = left_tree.grad (delta_l, l)
-        ((gTr, gMr, gbr), gWr) = right_tree.grad(delta_r, r)
+        ((gTl, gMl, gbl), gWl) = left_tree.grad (delta_l, l, s.hyp, param)
+        ((gTr, gMr, gbr), gWr) = right_tree.grad(delta_r, r, s.hyp, param)
         ((gT, gM, gb), gW) = ((gTl + gTr, gMl + gMr, gbl + gbr), gWl + gWr)
         
         return cost, (gS, (gT2, gM2, gb2), (gT, gM, gb), gW), np.argmax(softmax)
@@ -111,7 +113,6 @@ if __name__ == "__main__":
     hyp = HyperParameters()
     hyp.vocab_size = 100
     hyp.word_size = 2
-    hyp.composition_size = 3
     hyp.comparison_size = 4
     hyp.classes = 7
     hyp.composition_transfer = relu
@@ -120,12 +121,14 @@ if __name__ == "__main__":
     hyp.comparison_backtrans = relud
 
     param = Parameters()
-    param.vocab = dok_matrix((hyp.vocab_size, hyp.word_size))
-    param.composition = random_dtype(ntn_dtype(hyp.word_size, hyp.composition_size))
-    param.comparison  = random_dtype(ntn_dtype(hyp.composition_size, hyp.comparison_size))
-    param.softmax = np.random.randn(hyp.classes, hyp.comparison_size)
+    param.vocab = { 0: np.random.randn(hyp.word_size) }
+    param.composition = init_ntn_params(hyp.word_size, hyp.word_size)
+    param.comparison  = init_ntn_params(hyp.word_size, hyp.comparison_size)
+    param.softmax = np.random.randn(hyp.classes, hyp.comparison_size+1)
 
+    l = Leaf(0)
+    r = Tree(Tree(Leaf(0), Leaf(0)), Leaf(0))
 
-
-
+    sgd = SGD(hyp)
+    print sgd.cost_and_grad(l,r,2, param)
 
